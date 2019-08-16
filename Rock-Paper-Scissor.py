720a711128f95d6ca4dc843008866effb6b68be2
Harman Bhatia <harmanbhatia101@gmail.com>
2019-08-16 23:54:20 +0530

    Version 1.0.2

--- Rock-Paper-Scissor.py
+++ Rock-Paper-Scissor.py
@@ -1,9 +1,10 @@
 import random
 list_var = ["R", "P", "S"]
-p2 = random.choice(list_var)
+
 p = input("Enter your Name\n")
 while True:
-    p1 = input('''%s, Pick any one:
+    p2 = random.choice(list_var)
+    p1 = input('''Hey %s, Pick any one:
                    R for ROCK
                    P for PAPER
                    S for SCISSOR\n''' % p)
@@ -12,38 +13,38 @@ while True:
         if p1 != p2:
             if p1 == "R" and p2 == "S":
                 print("!!! You Win !!!")
-                if str(input('Do you want to play another game, yes or no?\n')) == 'yes':
+                if str(input('Do you want to play another game, if yes, Enter Y \n')) == 'Y':
                     continue
                 else:
                     print('game over.')
                     break
             elif p1 == "S" and p2 == 'P':
                 print("!!! You Win !!!")
-                if str(input('Do you want to play another game, yes or no?\n')) == 'yes':
+                if str(input('Do you want to play another game, if yes, Enter Y \n')) == 'Y':
                     continue
                 else:
                     print('game over.')
                     break
             elif p1 == 'P' and p2 == 'R':
                 print("!!! You Win !!!")
-                if str(input('Do you want to play another game, yes or no?\n')) == 'yes':
+                if str(input('Do you want to play another game, if yes, Enter Y \n')) == 'Y' or 'y':
                     continue
                 else:
                     print('game over.')
                     break
             else:
                 print("!!! You Lose !!!")
-                if str(input('Do you want to play another game, yes or no?\n')) == 'yes':
+                if str(input('Do you want to play another game, if yes, Enter Y\n')) == 'Y' or 'y':
                     continue
                 else:
                     print('game over.')
                     break
         else:
             print("It's a Tie !!")
-            if str(input('Do you want to play another game, yes or no?\n')) == 'yes':
+            if str(input('Do you want to play another game,if yes, Enter Y\n')) == 'Y' or 'y':
                 continue
             else:
-                print('game over.')
+                print('Game Over.')
                 break
     else:
         print("You need to learn the game kiddo. !! Enter the VALID input !! ")
